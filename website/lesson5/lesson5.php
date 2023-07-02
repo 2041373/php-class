449474c4f1b8345559262afd84755bc90c4050b2
nazar zaheer <73424320+2041373@users.noreply.github.com>
2023-07-02 16:39:20 +0100

    lesson5
    complete

--- website/lesson5/lesson5.php
+++ website/lesson5/lesson5.php
@@ -21,8 +21,29 @@
         <input type="text" name="y" id="y"><br>
         <input type="submit" value="Calculate">
     </form>
-    <p>This calculator rounds the value up to the nearest integer</p>
+    <p>This calculator rounds the value up to the nearest integer <br>
+       there are many more functions that can be used with numbers <br>
+       for example: floor (rounds down), ceil (rounds up) </p>
     
+    <h1>Power Calculator</h1>
+    <form action="lesson5.php" method="post">
+        <label>base</label><br>
+        <input type="number" name="base" id="base"><br>
+        <label>exponent</label><br>
+        <input type="number" name="exponent" id="exponent"><br>
+        <input type="submit" value="Calculate">
+    </form>
+
+    <h1>Random Number Generator</h1>
+    <form action="lesson5.php" method="post">
+        <label>min</label><br>
+        <input type="number" name="min" id="min"><br>
+        <label>max</label><br>
+        <input type="number" name="max" id="max"><br>
+        <input type="submit" value="Calculate">
+
+    <br>
+
 </body>
 </html>
 
@@ -37,5 +58,19 @@
   $total1 = null;
   $total1 = round($y); // absolute value of y
   echo "Round Calculator: The total is: $total1 <br>";
+
+  $base =  $_POST["base"];
+  $exponent =  $_POST["exponent"];
+  $total2 = null;
+  $total2 = pow($base, $exponent); 
+  echo "Power Calculator: The total is: $total2 <br>";
+
+  $min =  $_POST["min"];
+  $max =  $_POST["max"];
+  $total3 = null;
+  $total3 = rand($min, $max);
+  echo "Random Number Generator: The total is: $total3 <br>";
+
+
 
 ?> 
